c9e8a8f88ab0131365e98080150892dee7b0bf12
Mike A <michaelalhilly@gmail.com>
2022-05-18 07:07:25 +0000

    Multi page not working.

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script src="./script.js"></script>
+<style lang="stylus">@import "./style.styl";</style>
+<template lang="pug">
+div.greetings
+  h1.green {{ msg }}
+  h3
+    span You’ve successfully created a project with 
+    a(target="_blank" href="https://vitejs.dev/") Vite
+    span  + 
+    a(target="_blank" href="https://vuejs.org/") Vue 3
+    span .
+</template>
